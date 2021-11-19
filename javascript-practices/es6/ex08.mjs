011ba11be78b138cbd9139984d163c03e08bfcc2
hanjoohyung <ghd167@naver.com>
2021-11-19 18:05:44 +0900

    async

--- javascript-practices/es6/ex08.mjs
+++ javascript-practices/es6/ex08.mjs
@@ -14,13 +14,12 @@ console.log(m.add(10, 20));
 console.log(m.substract(10, 20));
 
 // 3.
-import {add, substract} from './ex08.04.mjs';
+import {substract} from './ex08.04.mjs';
 
-console.log(add(20, 60));
 console.log(substract(20, 60));
 
 // 4.
-import math, {add, substract} from './ex08.05.mjs';
+import math, {add} from './ex08.05.mjs';
 
-console.log(math.add(20, 10), math.substract(10, 20));
-console.log(add(30,20), substract(50, 30));
+console.log(math.substract(10, 20));
+console.log(add(30,20));
