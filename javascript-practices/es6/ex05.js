a46322b385926dbe0f1da69bf5bd119ccf63a5d8
hanjoohyung <ghd167@naver.com>
2021-11-19 14:19:40 +0900

    es06

--- javascript-practices/es6/ex05.js
+++ javascript-practices/es6/ex05.js
@@ -29,3 +29,32 @@ numbers.forEach(function(n) {
 });
 
 // ex2
+console.log("\n------------------------------------");
+numbers.forEach(n => process.stdout.write(`${n}:${power(n)}\t`));
+
+// ex3
+console.log("\n------------------------------------");
+[5, 3, 15, 1045, 43, 92].forEach(e => {
+    if(e%5 ==0) {
+        process.stdout.write(`${e}\t`);
+    }
+})
+
+// ex4
+console.log("\n------------------------------------");
+const dooly = {
+    name: '둘리',
+    friends: ['또치', '마이콜', '도우너', '홍길동'],
+    printFriedns: function() {
+        // this.friends.forEach(function(f) {
+        //     console.log(`${this.name}의 친구${f}`);
+        // }.bind(this));
+
+        this.friends.forEach(f => console.log(`${this.name}의 친구 ${f}`));
+    }
+}
+
+dooly.printFriedns();
+
+// ex5
+console.log("\n------------------------------------");
